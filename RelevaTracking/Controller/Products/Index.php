66224d2063ad02ac013bae054e116b764b800bdc
mba <mba@mba-Aspire-E5-575>
2019-04-03 08:52:34 +0200

    begin with products-export

--- RelevaTracking/Controller/Products/Index.php
+++ RelevaTracking/Controller/Products/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by:
  * User: Oleg G
@@ -12,104 +13,125 @@ namespace Relevanz\Tracking\Controller\Products;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\CatalogInventory\Model\Stock;
 
-class Index extends \Magento\Framework\App\Action\Action{
+class Index extends \Magento\Framework\App\Action\Action {
 
     /**
      * @var \Relevanz\Tracking\Model\Products
      */
-    protected $_productsModel;
+    private $productsModel;
+    
+    private $storeManager;
+    
+    private $helper;
 
-
-  
     /**
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Relevanz\JsonExport\Model\Products $productsModel
      */
     public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Relevanz\Tracking\Model\Products $productsModel
-    )
-    {
+            \Relevanz\Tracking\Model\Products $productsModel,
+            \Relevanz\Tracking\Helper\Data $helper,
+            \Magento\Store\Model\StoreManagerInterface $storeManager,
+            \Magento\Framework\App\Action\Context $context
+    ) {
+        $this->productsModel = $productsModel;
+        $this->storeManager = $storeManager;
+        $this->helper = $helper;
         parent::__construct($context);
-        $this->_productsModel       = $productsModel;
 //        $this->galleryReadHandler   = $galleryReadHandler;
     }
 
     /**
      * @return int
      */
-    protected function _getStoreId()
-    {
-        return (int)$this->getRequest()->getParam('store', 0);
+    protected function _getStoreId() {
+        return (int) $this->storeManager->getStore()->getId();
     }
 
     /**
      * @param $product
      */
-    protected function _addGallery($product)
-    {
-        $this->galleryReadHandler->execute($product);
-    }
+//    protected function _addGallery($product) {
+//        $this->galleryReadHandler->execute($product);
+//    }
 
     /**
      * @param $product
      * @return array
      */
-    protected function _getProductImages($product){
-        $images = array();
-        if($product instanceof \Magento\Catalog\Model\Product\Interceptor){
-//            $this->_addGallery($product);
-            $gallery = $product->getMediaGalleryImages();
-            if(count($gallery)){
-                foreach ($gallery as $item) {
-                    $images[] = $item->getUrl();
-                }
+//    protected function _getProductImages($product) {
+//        $images = array();
+//        if ($product instanceof \Magento\Catalog\Model\Product\Interceptor) {
+////            $this->_addGallery($product);
+//            $gallery = $product->getMediaGalleryImages();
+//            if (count($gallery)) {
+//                foreach ($gallery as $item) {
+//                    $images[] = $item->getUrl();
+//                }
+//            }
+//        }
+//        return $images;
+//    }
+    
+    private function getProducts($storeId) {
+        $result = [];
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $collection = $this->productsModel->getCollection($storeId);
+        if ($collection) {
+            foreach ($collection as $product) {
+                $product->setStoreId($storeId);
+                $product = $objectManager->create('Magento\Catalog\Model\Product')->load($product->getId());
+                $result[] = array(
+                    'product_id' => (int) $product->getId(),
+                    'category_ids' => implode(",", $product->getCategoryIds()),
+                    'product_name' => $product->getName(),
+                    'short_description' => $product->getShortDescription(),
+                    'price' => $product->getPrice(),
+//                        'images' => $this->_getProductImages($product),
+                    'stock_status' => ((((int) $product->getStockStatus()) == Stock::STOCK_IN_STOCK) ? 'IN_STOCK' : 'OUT_OF_STOCK')
+                );
             }
         }
-        return $images;
+        return $result;
     }
 
     /**
      * @return \Magento\Framework\Controller\ResultInterface
      */
-    public function execute()
-    {
-        $result       =   array();
+    public function execute() {
+        if (!$this->helper->isAuthed($this->getRequest()->getParam('auth', ''))) {
+            throw new \Exception('Not authed');
+        }
         try {
             $storeId = $this->_getStoreId();
-			$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-            $collection = $this->_productsModel->getCollection($storeId);
-            if ($collection) {
-                foreach ($collection as $product) {
-                    $product->setStoreId($storeId);
-				    $product = $objectManager->create('Magento\Catalog\Model\Product')->load($product->getId());        
-                    $result[] = array(
-                        'product_id'        =>  (int)$product->getId(),
-                        'category_ids'      =>  implode(",", $product->getCategoryIds()),
-                        'product_name'      =>  $product->getName(),
-                        'short_description' =>  $product->getShortDescription(),
-                        'price'             =>  $product->getPrice(),
-                        'images'            =>  $this->_getProductImages($product),
-                        'stock_status'      =>  (($product->getStockStatus() == Stock::STOCK_IN_STOCK) ? 'IN_STOCK' : 'OUT_OF_STOCK')
-                    );
-                }
+            $result = $this->getProducts($storeId);
+        } catch (Exception $e) {
+            $result = array(
+                'status' => 'ERROR',
+                'message' => $e->getMessage()
+            );
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+            $result = array(
+                'status' => 'ERROR',
+                'message' => $e->getMessage()
+            );
+        }
+        $type = $this->getRequest()->getParam('type', 'json');
+        $type = in_array($type, ['json', 'csv']) ? $type : 'json';
+        if ($type === 'json') {
+            $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+            $response->setData($result);
+            return $response;
+        } else/*if ($type === 'csv')*/ {
+            $response = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+            $stream = fopen('data://text/plain,' . "", 'w+');
+            foreach ($result as $val) {
+                fputcsv($stream, $val);
             }
+            rewind($stream);
+            $response->setContents( stream_get_contents($stream));
+            return $response;
         }
-        catch(Exception $e){
-            $result = array(
-                'status'    =>  'ERROR',
-                'message'   =>  $e->getMessage()
-            );
-        }
-        catch(\Magento\Framework\Exception\NoSuchEntityException $e){
-            $result = array(
-                'status'    =>  'ERROR',
-                'message'   =>  $e->getMessage()
-            );
-        }
-        $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-        $response->setData($result);
-        return $response;
     }
 
 }
--- RelevaTracking/Helper/Data.php
+++ RelevaTracking/Helper/Data.php
@@ -17,67 +17,54 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     const XML_PATH_TRACKING_FRONT_PAGE      = 'relevanz_tracking/tracking/front_page_enabled';
     const XML_PATH_TRACKING_FRONT_CATEGORY  = 'relevanz_tracking/tracking/category_page_enabled';
     const XML_PATH_TRACKING_FRONT_PRODUCT   = 'relevanz_tracking/tracking/product_page_enabled';
-    const XML_PATH_TRACKING_SUCCESS_PAGE    = 'relevanz_tracking/tracking/order_success_page_enabled';
+    const XML_PATH_TRACKING_FRONT_SUCCESS_PAGE    = 'relevanz_tracking/tracking/order_success_page_enabled';
 
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected $_scopeConfig;
-
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $_storeManager;
-
-	/**
-     * @param \Magento\Framework\App\Helper\Context $context
-     */
-    public function __construct(
-        \Magento\Framework\App\Helper\Context $context
-    ) {
-        parent::__construct($context);
-        $this->_scopeConfig     = $this->scopeConfig;
+    public function isAuthed($auth) {
+        return 
+            $this->isEnabled()
+            && md5($this->scopeConfig->getValue(self::XML_PATH_API_KEY, \Magento\Store\Model\ScopeInterface::SCOPE_STORE).':'.((string) $this->getClientId())) == $auth
+        ;
     }
 
     /**
      * @return bool
      */
     public function isEnabled(){
-        return (bool)$this->_scopeConfig->getValue(self::XML_PATH_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return (bool)$this->scopeConfig->getValue(self::XML_PATH_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     /**
      * @return string
      */
     public function getClientId(){
-        return (string)$this->_scopeConfig->getValue(self::XML_PATH_CLIENT_ID, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_CLIENT_ID, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     /**
      * @return string
      */
     public function isFrontPageTrackEnabled(){
-        return (string)$this->_scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_PAGE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_PAGE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     /**
      * @return string
      */
     public function isCategoryTrackEnabled(){
-        return (string)$this->_scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_CATEGORY, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_CATEGORY, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     /**
      * @return string
      */
     public function isProductTrackEnabled(){
-        return (string)$this->_scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_PRODUCT, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_PRODUCT, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
     /**
      * @return string
      */
     public function isSuccessPageTrackEnabled(){
-        return (string)$this->_scopeConfig->getValue(self::XML_PATH_TRACKING_SUCCESS_PAGE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_TRACKING_FRONT_SUCCESS_PAGE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 }
